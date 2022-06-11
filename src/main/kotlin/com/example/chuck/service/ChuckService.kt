2e16a38dc8d262610cc963fa67b1472ff3aec8b2
Ernesto Arroyo <earroyo@heytrade.com>
2022-06-11 12:06:43 +0200

    Intentamos poner a chuck pero no lo inyecta! why?
    no hemos dicho a Spring que meta a chuck en el contexto!

--- src/main/kotlin/com/example/chuck/service/ChuckService.kt
+++ src/main/kotlin/com/example/chuck/service/ChuckService.kt
@@ -2,9 +2,10 @@ package com.example.chuck.service
 
 import com.example.chuck.controller.JokesService
 import org.springframework.stereotype.Service
-//import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-
+import guru.springframework.norris.chuck.ChuckNorrisQuotes
 @Service
-class ChuckNorrisJokes : JokesService {
-    override fun getJoke() = "Hahaha"
+class ChuckJokes(val jokes: ChuckNorrisQuotes ) : JokesService {
+    override fun getJoke(): String {
+       return jokes.randomQuote
+    }
 }
