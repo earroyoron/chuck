376531f5a6a1309fb5037df26c73ce41ec2dc4ca
Ernesto Arroyo <earroyo@heytrade.com>
2022-06-11 11:42:39 +0200

    Better with a proper constructor injection!

--- src/main/kotlin/com/example/chuck/controller/ChuckController.kt
+++ src/main/kotlin/com/example/chuck/controller/ChuckController.kt
@@ -5,9 +5,7 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class ChuckController() {
-
-    lateinit var jokesService: JokesService
+class ChuckController(val jokesService: JokesService) {
 
     @RequestMapping("")
     fun funnyQuote(model: Model): String {
