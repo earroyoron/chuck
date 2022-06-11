6ce89fc4849f9637f6d1a32fd90e66804119cae1
Ernesto Arroyo <earroyo@heytrade.com>
2022-06-11 12:08:59 +0200

    Ahora sí!

--- src/main/kotlin/com/example/chuck/ChuckApplication.kt
+++ src/main/kotlin/com/example/chuck/ChuckApplication.kt
@@ -1,11 +1,22 @@
 package com.example.chuck
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class ChuckApplication
 
 fun main(args: Array<String>) {
 	runApplication<ChuckApplication>(*args)
+}
+
+@Configuration
+class AppConfiguration {
+
+	@Bean
+	fun chuckNorrisBean() = ChuckNorrisQuotes()
+
 }
